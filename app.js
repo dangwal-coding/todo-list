35cf77c1a3602e3a90d867a5c37447269704225c
dangwal-coding <bca23067@glbim.ac.in>
2024-07-11 21:39:21 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+let inp = document.querySelector("input");
+let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
+
+btn.addEventListener("click",function(){
+let item = document.createElement("li"); 
+item.innerText=inp.value;
+ul.appendChild(item);
+
+let delbtn = document.createElement("button"); 
+delbtn.innerText="delete";
+
+delbtn.classList.add("delete");
+item.appendChild(delbtn);
+inp.value="";
+});
+
+ul.addEventListener("click", function(event){
+ if(event.target.nodeName=="BUTTON"){
+    let listItem = event.target.parentElement;
+    listItem.remove();
+    console.log("deleted....");
+ }
+});
+
+// let delBtns = document.querySelectorAll(".delete");
+// for(delBtn of delBtns){
+//     delBtn.addEventListener("click",function(){
+//         let par = this.parentElement;
+//         console.log(par);
+//         par.remove();
+//     });
+// }
